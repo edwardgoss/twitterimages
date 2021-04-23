676a3df98c2b366883aa6289927adbffd53ba566
Edward Goss <edwardjamesgoss@gmail.com>
2021-04-23 14:46:22 -0400

    IP Update

--- server/api/twitter.js
+++ server/api/twitter.js
@@ -11,29 +11,6 @@ const client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
-// default page just to render something before anything is searched
-// router.route("/").get((req, res) => {
-//   const defaultItems = [
-//     "grand canyon",
-//     "yosemite",
-//     "yellowstone",
-//     "acadia",
-//     "zion",
-//     "bryce canyon",
-//     "grand teton",
-//   ];
-
-//   const randomItem =
-//     defaultItems[Math.floor(Math.random() * defaultItems.length)];
-//   client
-//     .get("search/tweets", {
-//       q: `${randomItem} -filter:retweets filter:images filter:safe`,
-//       count: 100,
-//     })
-//     .then((tweets) => res.json(tweets))
-//     .catch(console.error);
-// });
-
 router.route("/search/:query").get((req, res) => {
   const searchQuery = req.params.query;
   client
--- server/server.js
+++ server/server.js
@@ -11,25 +11,6 @@ const twitterRouter = require("./api/twitter");
 
 app.use("/", twitterRouter);
 
-// const Twitter = require("twitter-lite");
-// require("dotenv").config();
-
-// const client = new Twitter({
-//   subdomain: "api",
-//   version: "1.1",
-//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
-//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-//   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-// });
-
-// client
-//   .get("search/tweets", {
-//     q: "yosemite",
-//   })
-//   .then((tweets) => console.log(tweets))
-//   .catch(console.error);
-
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
